524600791966b32d49ca46d60124832491e5e57f
Dejan <dexterns88@gmail.com>
2019-03-27 23:40:03 +0100

    Create login auth

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+Use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'sasa',
+            'email' => 'sasans87@gmail.com',
+            'password' => bcrypt('123456'),
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,30 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+/*
+ * Auth router
+ */
+Route::post('login', 'UserController@authenticate');
+Route::get('refresh', 'UserController@refresh');
+
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+/*
+ * JWT protected
+ */
+Route::group(['middleware' => ['jwt.verify']], function() {
+    /**
+     * Protected get
+     */
+    Route::get('user', 'UserController@getAuthenticatedUser');
+//    Route::get('closed', 'DataController@closed');
+
+    /**
+     * Protected post methods
+     */
+    Route::post('register', 'UserController@register');
+//    Route::post('product/create', 'ProductController@createProduct');
+//    Route::post('product/promote', 'ProductController@promoteProduct');
 });
